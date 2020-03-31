c4148ec8fcb042226501ad23b84ecb7831228edd
Jason Cruz <jason1642@gmail.com>
2020-03-31 16:54:59 -0400

    some more work done

--- src/components/HomePageContainer/NewsColumnTwo/NewsColumnTwo.js
+++ src/components/HomePageContainer/NewsColumnTwo/NewsColumnTwo.js
@@ -11,42 +11,45 @@ class NewsColumnTwo extends Component {
         }
     }
 componentDidMount = async () => {
-        let topic = "tesla"
-        let arr = []
+    
         const API_KEY_TWO = "cce7827002dc4aacac0aa05b70ad0f33"
-        const responseTwo = await axios(`https://newsapi.org/v2/top-headlines?q=${topic}&apiKey=${API_KEY_TWO}`)
+        const responseTwo = await axios(`https://newsapi.org/v2/top-headlines?q=world&apiKey=${API_KEY_TWO}`)
+        console.log(this.state.list)
         this.setState({
-            list : responseTwo.data.articles.map(ele=><li>{ele.title}</li>)
-        })
-    }
+            list : responseTwo.data.articles.map(ele=><li className="col-two-li" onClick={()=>window.open(ele.url, "_blank")}>{ele.title}</li>)
+    })
+}
+   
     renderFunc = () => {
-        if(this.props.newsArr[1]) {
-            console.log(this.props.newsArr[1].multimedia[0].url)
+        
+        console.log(this.state.list)
             return (<>
                 <div className="col-two-img-div">
                 <img className="column-two-img" src={this.props.newsArr[1].multimedia[0].url} />
-                <div className="on-img-label">Opinion</div>
-                
+                <div className="on-img-label">Opinion</div>  
                 </div>
-            <h1 className="col-two-main-title">{this.props.newsArr[1].title}</h1>
+            <h2 className="col-two-main-title">{this.props.newsArr[1].title}</h2>
                 <br />
+                <div className="col-two-ul">
                 <ul>
-                    {this.state.list}
-                </ul>
+                    {this.state.list.slice(0,5)}
+                </ul></div>
                 <div className="col-two-story-two">
-                    <img src="https://cdn.cnn.com/cnnnext/dam/assets/200330112332-macys-herald-square-store-0329-restricted-large-tease.jpg" />
-                    <h2>Title 2 column 2</h2>
-                </div></>
+                    <img className="col-two-img-two" src={this.props.newsArr[2 ].multimedia[0].url} />
+                    <h2 className="col-two-main-title">{this.props.newsArr[2].title}</h2>
+                    <div className="col-two-ul2">
+                <ul>
+                    {this.state.list.slice(5,10)}
+                </ul></div>
+                </div>
+                </>
             )
-        } else {
-            return (<></>)
-        }
     }
     render() {
-        console.log(this.props.newsArr[0])
+        
     return (
         <div className="news-column-two">
-          {this.renderFunc()}
+          {this.props.newsArr[1] ? this.renderFunc() : <> </>}
         </div>
     )
     }
