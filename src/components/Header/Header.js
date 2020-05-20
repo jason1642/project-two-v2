708e2153d5b961fa7b837164ba7c58a573f8b9e2
jasonc1642 <jason1642@gmail.com>
2020-05-20 05:53:10 -0400

    added header title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,18 @@
 import React from "react"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import "./Header.css"
+// import '../images/penguinLogo.jpeg'
 
-const Header = () => {    
-    return(
-        <header>
-        
-      <img className="header-img" src="https://lh3.googleusercontent.com/proxy/Ah8Z7YHYgCPgbd_g8J53ffjFRodseUKLpQKt2HS6OcLNkCEZBRo1_pNJ6qe8ERkOE020J-6knzbKBUHgxLbZf-KhY-uUWsWI-wJ0ylJzK_0pZaLZkE7EHo2vBvS0SJtUNt60zXDxnDAx7IjL9CQ" />
+const Header = () => {
+  return (
+    <header>
+      <h1>Sky News</h1>
+      {/* <img className="header-img" src={penguinLogo.jpeg} /> */}
 
       <Link className="business-button-link" to="/business"><button className="business-button">Business</button></Link>
       <Link className="home-link" to="/">Home</Link>
     </header>
-    )
+  )
 }
 
 export default Header
